6a68de3c3afdb9d6ff748f60a97b8d589d626b74
Christopher D'Angelo <chris@mathhead200.com>
2026-01-09 18:10:22 -0500

    Update tools.py

--- MSD Research Project - Latest Development Version/dev/msd-compiler/src/mcheisenberg/tools.py
+++ MSD Research Project - Latest Development Version/dev/msd-compiler/src/mcheisenberg/tools.py
@@ -1,3 +1,4 @@
+import subprocess
 
 class Assembler:
 	def assemble(*src) -> list[str]:
@@ -10,16 +11,17 @@ class Linker:
 class VisualStudio(Assembler, Linker):
 	def __init__(self, year: int=2022, edition: str="Community", install: str=None, tool: str=None):
 		DEFAULTS = {  # (instal, tool)
-			2022: ("C:/Program Files/Microsoft Visual Studio/2022", "VC/Auxiliary/Build/vcvars64.bat"),
-			2019: ("C:/Program Files (x86)/Microsoft Visual Studio/2019", "VC/Auxiliary/Build/vcvars64.bat"),
-			2017: ("C:/Program Files (x86)/Microsoft Visual Studio/2017", "VC/Auxiliary/Build/vcvars64.bat"),
-			2015: ("C:/Program Files (x86)/Microsoft Visual Studio 14.0", "VC/vcvarsall.bat x64"),
-			2013: ("C:/Program Files (x86)/Microsoft Visual Studio 12.0", "VC/vcvarsall.bat x64")
+			2022: ("C:\\Program Files\\Microsoft Visual Studio\\2022", "VC\\Auxiliary\\Build\\vcvars64.bat"),
+			2019: ("C:\\Program Files (x86)\\Microsoft Visual Studio\\2019", "VC\\Auxiliary\\Build\\vcvars64.bat"),
+			2017: ("C:\\Program Files (x86)\\Microsoft Visual Studio\\2017", "VC\\Auxiliary\\Build\\vcvars64.bat"),
+			2015: ("C:\\Program Files (x86)\\Microsoft Visual Studio 14.0", "VC\\vcvarsall.bat x64"),
+			2013: ("C:\\Program Files (x86)\\Microsoft Visual Studio 12.0", "VC\\vcvarsall.bat x64")
 		}
 		if install is None:
 			intall, _ = DEFAULTS[year]
 		if tool is None:
 			_, tool = DEFAULTS[year]
-		self.cmd = f"{install}/{edition}/{tool}"
+		self.cmd = f"{install}\\{edition}\\{tool}"
 
-	
+	def assemble(self, *src):
+		subprocess.run(f'"{self.cmd}\" && ml64 /c ...files... ...output...', shell=True, check=True)
