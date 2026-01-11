2cd1efe55f016ca40e54917da84d99a666965ceb
Christopher D'Angelo <chris@mathhead200.com>
2026-01-11 17:01:22 -0500

    "

--- MSD Research Project - Latest Development Version/dev/msd-compiler/src/mcheisenberg/runtime.py
+++ MSD Research Project - Latest Development Version/dev/msd-compiler/src/mcheisenberg/runtime.py
@@ -1,11 +1,11 @@
 from .driver import Driver
 import os
 
-class Runtime:
+class Runtime(Driver):
 	def __init__(self, dll: str, delete: bool=False):
+		super().__init__(dll)
 		self.dll = dll
 		self.delete = delete  # delete on exit?
-		self.driver = Driver(dll)
 
 	def __enter__(self):
 		pass  # TODO: (stub)
